866fab1bf4b1927e3261c3b579f6e451b3ef7978
Sunbridger <739272884@qq.com>
2019-10-16 15:08:32 +0800

    egg curd的操作

--- app/controller/home.js
+++ app/controller/home.js
@@ -17,6 +17,32 @@ class HomeController extends Controller {
         ctx.status = 201;
         ctx.body = user;
     }
+    async update() {
+      const ctx = this.ctx;
+      const id = toInt(ctx.params.id);
+      const user = await ctx.model.User.findByPk(id);
+      if (!user) {
+        ctx.status = 404;
+        return;
+      }
+  
+      const { name, age } = ctx.request.body;
+      await user.update({ name, age });
+      ctx.body = user;
+    }
+  
+    async destroy() {
+      const ctx = this.ctx;
+      const id = toInt(ctx.params.id);
+      const user = await ctx.model.User.findByPk(id);
+      if (!user) {
+        ctx.status = 404;
+        return;
+      }
+  
+      await user.destroy();
+      ctx.status = 200;
+    }
 }
 
 module.exports = HomeController;
