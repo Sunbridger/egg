e03bef8c1006c7f90d8b909d00c700ee94ba2e46
Sunbridger <739272884@qq.com>
2019-10-31 14:23:16 +0800

    feat: 连接数据库3

--- app/controller/users.js
+++ app/controller/users.js
@@ -9,48 +9,15 @@ function toInt(str) {
 class UserController extends Controller {
   async index() {
     const ctx = this.ctx;
-    const query = { limit: toInt(ctx.query.limit), offset: toInt(ctx.query.offset) };
-    ctx.body = await ctx.model.User.findAll(query);
-  }
-
-  async show() {
-    const ctx = this.ctx;
-    ctx.body = await ctx.model.User.findByPk(toInt(ctx.params.id));
+    ctx.body = await ctx.model.User.findAll();
   }
 
   async create() {
     const ctx = this.ctx;
-    const { name, age } = ctx.request.body;
+    const { name, age } = ctx.request.query;
     const user = await ctx.model.User.create({ name, age });
     ctx.status = 201;
     ctx.body = user;
-  }
-
-  async update() {
-    const ctx = this.ctx;
-    const id = toInt(ctx.params.id);
-    const user = await ctx.model.User.findByPk(id);
-    if (!user) {
-      ctx.status = 404;
-      return;
-    }
-
-    const { name, age } = ctx.request.body;
-    await user.update({ name, age });
-    ctx.body = user;
-  }
-
-  async destroy() {
-    const ctx = this.ctx;
-    const id = toInt(ctx.params.id);
-    const user = await ctx.model.User.findByPk(id);
-    if (!user) {
-      ctx.status = 404;
-      return;
-    }
-
-    await user.destroy();
-    ctx.status = 200;
   }
 }
 
