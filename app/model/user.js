afa04c607465a17e031c3d1d4abe8d7b414dba5b
Sunbridger <739272884@qq.com>
2019-10-31 16:32:17 +0800

    feat: 修改格式吧

--- app/model/user.js
+++ app/model/user.js
@@ -1,6 +1,6 @@
 module.exports = app => {
     const { STRING, INTEGER, DATE } = app.Sequelize;
-    const User = app.model.define('user', {
+    const User = app.model.define('User', {
         id: { type: INTEGER, primaryKey: true, autoIncrement: true },
         name: STRING(30),
         age: INTEGER,
--- app/schedule/clear.js
+++ app/schedule/clear.js
@@ -1,10 +1,10 @@
 module.exports = {
     schedule: {
-      interval: '1m', // 1 分钟间隔
+      interval: '60m', // 1 分钟间隔
       type: 'all', // 指定所有的 worker 都需要执行
       immediate: true
     },
     async task(ctx) {
-        console.log(1, '------- 定时器任务-----');
+        ctx.logger.info('定时器任务执行了');
     }
 };
