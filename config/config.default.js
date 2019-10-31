4058da93bb0a86d3bec69ed2c53c540a85b90e3b
Sunbridger <739272884@qq.com>
2019-10-31 17:49:45 +0800

    feat: 数据库密码

--- config/config.default.js
+++ config/config.default.js
@@ -8,7 +8,7 @@ module.exports = appInfo => {
     host: '127.0.0.1',
     port: 3306,
     username: 'root',
-    // password: '123456',
+    password: '123456',
     database: 'egg',
   };
   return {
