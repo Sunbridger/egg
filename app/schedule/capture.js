d8a9f9ba175d16c5eb90c07b2537b9cc61d56e44
Sunbridger <739272884@qq.com>
2019-11-18 21:25:43 +0800

    feat: 修改

--- app/schedule/capture.js
+++ app/schedule/capture.js
@@ -1,25 +1,25 @@
-const puppeteer = require('puppeteer');
-const path = p => require('path').resolve(__dirname, '../../assets', p);
-async function scrape(imgname) {
-    const browser = await puppeteer.launch({
-        headless: true,
-        dumpio: false,
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
-    });
-    const page = await browser.newPage();
-    await page.goto('https://m.weibo.cn/p/index?containerid=106003type%3D25%26t%3D3%26disable_hot%3D1%26filter_type%3Drealtimehot&title=%E5%BE%AE%E5%8D%9A%E7%83%AD%E6%90%9C', {
-        timeout: 3000000
-    });
-    await page.setViewport({
-        width: 375,
-        height: 667
-    });
-    await page.screenshot({
-        path: path(imgname),
-        fullPage: true
-    });
-    await browser.close();
-}
+// const puppeteer = require('puppeteer');
+// const path = p => require('path').resolve(__dirname, '../../assets', p);
+// async function scrape(imgname) {
+//     const browser = await puppeteer.launch({
+//         headless: true,
+//         dumpio: false,
+//         args: ['--no-sandbox', '--disable-setuid-sandbox']
+//     });
+//     const page = await browser.newPage();
+//     await page.goto('https://m.weibo.cn/p/index?containerid=106003type%3D25%26t%3D3%26disable_hot%3D1%26filter_type%3Drealtimehot&title=%E5%BE%AE%E5%8D%9A%E7%83%AD%E6%90%9C', {
+//         timeout: 3000000
+//     });
+//     await page.setViewport({
+//         width: 375,
+//         height: 667
+//     });
+//     await page.screenshot({
+//         path: path(imgname),
+//         fullPage: true
+//     });
+//     await browser.close();
+// }
 
 
 module.exports = {
