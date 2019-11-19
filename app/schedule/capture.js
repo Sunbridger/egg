caa8f072c37894e56f3a4f6e20a0cf80ff81bae8
Sunbridger <739272884@qq.com>
2019-11-19 15:35:25 +0800

    fix: xiugaidizhi

--- app/schedule/capture.js
+++ app/schedule/capture.js
@@ -7,7 +7,7 @@ async function scrape(imgname) {
         args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
     const page = await browser.newPage();
-    await page.goto('https://h5.m.taobao.com/', {
+    await page.goto('https://m.weibo.cn/p/index?containerid=106003type%3D25%26t%3D3%26disable_hot%3D1%26filter_type%3Drealtimehot&title=%E5%BE%AE%E5%8D%9A%E7%83%AD%E6%90%9C', {
         timeout: 3000000
     });
     await page.setViewport({
