f94978c84e3148cb08c31a222272ac8524a4b732
Sunbridger <739272884@qq.com>
2019-11-24 20:31:59 +0800

    fix: 修改timeout

--- app/schedule/watchTB.js
+++ app/schedule/watchTB.js
@@ -12,7 +12,9 @@ async function watchTB(good_url, tit_price) {
         height: 667
     });
     await page.setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1');
-    await page.goto(good_url);
+    await page.goto(good_url, {
+        timeout: 300000
+    });
     await page.content();
     await page.waitFor(10000);
     const result = await page.evaluate(() => {
--- app/service/puppeteer.js
+++ app/service/puppeteer.js
@@ -13,8 +13,11 @@ async function getTBDeatil(good_url, timewait = 10000) {
         height: 667
     });
     await page.setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1');
-    await page.goto(good_url);
+    await page.goto(good_url, {
+        timeout: 300000
+    });
     await page.waitFor(timewait);
+    console.log('✅ 正常打开了页面了啊');
     const result = await page.evaluate(() => {
         let tit_price = document.querySelector('.real-price').innerText;
         let good_title = document.querySelector('.main').innerText;
