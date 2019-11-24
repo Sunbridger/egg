23e4fd45ea16cde283eee053229ce6625cd519e0
Sunbridger <739272884@qq.com>
2019-11-24 20:42:03 +0800

    fix: test

--- app/service/puppeteer.js
+++ app/service/puppeteer.js
@@ -3,25 +3,28 @@ const puppeteer = require('puppeteer');
 
 async function getTBDeatil(good_url, timewait = 10000) {
     const browser = await puppeteer.launch({
-        headless: false,
+        headless: true,
         dumpio: false,
         args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
     const page = await browser.newPage();
-    await page.setViewport({
-        width: 375,
-        height: 667
-    });
-    await page.setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1');
+    // await page.setViewport({
+    //     width: 375,
+    //     height: 667
+    // });
+    // await page.setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1');
     await page.goto(good_url, {
         timeout: 300000
     });
     await page.waitFor(timewait);
     console.log('✅ 正常打开了页面了啊');
     const result = await page.evaluate(() => {
-        let tit_price = document.querySelector('.real-price').innerText;
-        let good_title = document.querySelector('.main').innerText;
-        let good_img = document.querySelector('.scroller.preview-scroller > a:nth-child(2) > img').src;
+        let tit_price = document.querySelector('#J_PromoPrice > dd > div > span').innerText;
+        let good_title = document.querySelector('#J_DetailMeta > div.tm-clear > div.tb-property > div > div.tb-detail-hd > h1').innerText;
+        let good_img = document.querySelector('#J_ImgBooth').src;
+        // let tit_price = document.querySelector('.real-price').innerText;
+        // let good_title = document.querySelector('.main').innerText;
+        // let good_img = document.querySelector('.scroller.preview-scroller > a:nth-child(2) > img').src;
 
         return {
             tit_price,
@@ -29,8 +32,8 @@ async function getTBDeatil(good_url, timewait = 10000) {
             good_img
         };
     });
-    // await page.close();
-    // await browser.close();
+    await page.close();
+    await browser.close();
     result.good_url = good_url;
     return result;
 }
