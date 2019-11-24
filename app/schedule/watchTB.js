9690adc091e00e4b4ab69524b90213313fc6e009
Sunbridger <739272884@qq.com>
2019-11-24 17:14:34 +0800

    fix: 修改时间

--- app/schedule/watchTB.js
+++ app/schedule/watchTB.js
@@ -39,7 +39,7 @@ module.exports = app => {
         schedule: {
             interval: '2m', // 6分钟间隔
             type: 'all', // 指定所有的 worker 都需要执行
-            immediate: true
+            // immediate: true
         },
         async task(ctx) {
             const taobaos = await ctx.model.Taobao.findAll();
