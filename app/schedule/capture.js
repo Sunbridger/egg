263ee1817ab7bfd2ba02d79dd79dba8c0006699f
Sunbridger <739272884@qq.com>
2019-11-24 22:57:17 +0800

    fix: test

--- app/schedule/capture.js
+++ app/schedule/capture.js
@@ -1,46 +1,46 @@
-// const puppeteer = require('puppeteer');
-// const path = p => require('path').resolve(__dirname, '../../assets', p);
-// async function gethotkey() {
-//     const browser = await puppeteer.launch({
-//         headless: true,
-//         dumpio: false,
-//         args: ['--no-sandbox', '--disable-setuid-sandbox']
-//     });
-//     const page = await browser.newPage();
-//     await page.goto('https://m.weibo.cn/search?containerid=231583', {
-//         timeout: 3000000
-//     });
-//     await page.content();
-//     await page.waitFor(3000);
-//     const result = await page.evaluate(() => {
-//         let hots = Array.from(document.querySelectorAll('#app > div:nth-child(1) > div:nth-child(1) > div.card.m-panel.card16.m-col-2 > div > div .m-item-box .m-text-cut')).map(ele => ele.innerText);
-//         hots.pop();
-//         return hots;
-//     });
-//     await page.close();
-//     await browser.close();
-//     return result;
-// }
+const puppeteer = require('puppeteer');
+const path = p => require('path').resolve(__dirname, '../../assets', p);
+async function gethotkey() {
+    const browser = await puppeteer.launch({
+        headless: true,
+        dumpio: false,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
+    const page = await browser.newPage();
+    await page.goto('https://m.weibo.cn/search?containerid=231583', {
+        timeout: 3000000
+    });
+    await page.content();
+    await page.waitFor(3000);
+    const result = await page.evaluate(() => {
+        let hots = Array.from(document.querySelectorAll('#app > div:nth-child(1) > div:nth-child(1) > div.card.m-panel.card16.m-col-2 > div > div .m-item-box .m-text-cut')).map(ele => ele.innerText);
+        hots.pop();
+        return hots;
+    });
+    await page.close();
+    await browser.close();
+    return result;
+}
 
-// module.exports = app => {
-//     return {
-//         schedule: {
-//             interval: '5m', // 6分钟间隔
-//             type: 'all' // 指定所有的 worker 都需要执行
-//         },
-//         async task(ctx) {
-//             const hots = await gethotkey();
-//             hots.forEach(async text => {
-//                 const hasText = await ctx.model.Hots.findByPk(text);
-//                 if (!hasText) {
-//                     await ctx.model.Hots.create({text});
-//                 } else {
-//                     const num = hasText.dataValues.num + 1;
-//                     await hasText.update({
-//                         num
-//                     });
-//                 }
-//             })
-//         }
-//     }
-// };
+module.exports = app => {
+    return {
+        schedule: {
+            interval: '5m', // 6分钟间隔
+            type: 'all' // 指定所有的 worker 都需要执行
+        },
+        async task(ctx) {
+            // const hots = await gethotkey();
+            // hots.forEach(async text => {
+            //     const hasText = await ctx.model.Hots.findByPk(text);
+            //     if (!hasText) {
+            //         await ctx.model.Hots.create({text});
+            //     } else {
+            //         const num = hasText.dataValues.num + 1;
+            //         await hasText.update({
+            //             num
+            //         });
+            //     }
+            // })
+        }
+    }
+};
--- app/schedule/watchTB.js
+++ app/schedule/watchTB.js
@@ -1,65 +1,65 @@
-// const puppeteer = require('puppeteer');
-// const path = p => require('path').resolve(__dirname, '../../assets', p);
-// async function watchTB(good_url, tit_price) {
-//     const browser = await puppeteer.launch({
-//         headless: true,
-//         dumpio: false,
-//         args: ['--no-sandbox', '--disable-setuid-sandbox']
-//     });
-//     const page = await browser.newPage();
-//     await page.setViewport({
-//         width: 375,
-//         height: 667
-//     });
-//     await page.setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1');
-//     await page.goto(good_url, {
-//         timeout: 300000
-//     });
-//     await page.content();
-//     await page.waitFor(10000);
-//     const result = await page.evaluate(() => {
-//         console.log(document.querySelector('.real-price'),'sadadas');
-//         let new_price = document.querySelector('.real-price').innerText;
-//         return {
-//             new_price
-//         };
-//     });
-//     await page.close();
-//     await browser.close();
-//     if (result.new_price !== tit_price) {
-//         console.log('❌ 价格不一样了');
-//         return {
-//             good_url,
-//             new_price: result.new_price
-//         }
-//     }
-//     console.log('✅ 正常结束咯');
-// }
+const puppeteer = require('puppeteer');
+const path = p => require('path').resolve(__dirname, '../../assets', p);
+async function watchTB(good_url, tit_price) {
+    const browser = await puppeteer.launch({
+        headless: true,
+        dumpio: false,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
+    const page = await browser.newPage();
+    await page.setViewport({
+        width: 375,
+        height: 667
+    });
+    await page.setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1');
+    await page.goto(good_url, {
+        timeout: 300000
+    });
+    await page.content();
+    await page.waitFor(10000);
+    const result = await page.evaluate(() => {
+        console.log(document.querySelector('.real-price'),'sadadas');
+        let new_price = document.querySelector('.real-price').innerText;
+        return {
+            new_price
+        };
+    });
+    await page.close();
+    await browser.close();
+    if (result.new_price !== tit_price) {
+        console.log('❌ 价格不一样了');
+        return {
+            good_url,
+            new_price: result.new_price
+        }
+    }
+    console.log('✅ 正常结束咯');
+}
 
-// module.exports = app => {
-//     return {
-//         schedule: {
-//             interval: '10',
-//             type: 'all'
-//         },
-//         async task(ctx) {
-//             const taobaos = await ctx.model.Taobao.findAll();
-//             if (taobaos.length) {
-//                 let needUpdateArr = await Promise.all(taobaos.map(async good => {
-//                     const { good_url, tit_price } = good.dataValues;
-//                     return await watchTB(good_url, tit_price);
-//                 }));
-//                 needUpdateArr = needUpdateArr.filter(el => el);
-//                 needUpdateArr.forEach(async good => {
-//                     const thisgood = await ctx.model.Taobao.findByPk(good.good_url);
-//                     await thisgood.update({
-//                         new_price: `${thisgood.dataValues.new_price},${good.new_price}`
-//                     });
-//                     console.log('✅ 更新完成')
-//                 });
-//             } else {
-//                 console.log('暂无watch的😁');
-//             }
-//         }
-//     }
-// };
+module.exports = app => {
+    return {
+        schedule: {
+            interval: '10',
+            type: 'all'
+        },
+        async task(ctx) {
+            // const taobaos = await ctx.model.Taobao.findAll();
+            // if (taobaos.length) {
+            //     let needUpdateArr = await Promise.all(taobaos.map(async good => {
+            //         const { good_url, tit_price } = good.dataValues;
+            //         return await watchTB(good_url, tit_price);
+            //     }));
+            //     needUpdateArr = needUpdateArr.filter(el => el);
+            //     needUpdateArr.forEach(async good => {
+            //         const thisgood = await ctx.model.Taobao.findByPk(good.good_url);
+            //         await thisgood.update({
+            //             new_price: `${thisgood.dataValues.new_price},${good.new_price}`
+            //         });
+            //         console.log('✅ 更新完成')
+            //     });
+            // } else {
+            //     console.log('暂无watch的😁');
+            // }
+        }
+    }
+};
