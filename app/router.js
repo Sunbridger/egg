d3c4704323d483273ec136b1100ae42a2081a938
Sunbridger <739272884@qq.com>
2019-12-06 11:20:55 +0800

    fix: 去除/匹配

--- app/router.js
+++ app/router.js
@@ -2,7 +2,7 @@
 
 module.exports = app => {
     const { router, controller } = app;
-    router.get('/', controller.user.get);
+    // router.get('/', controller.user.get);
     router.get('/add', controller.user.create);
     router.get('/me', controller.user.getme);
     router.get('/mall', controller.user.gettaobao);
