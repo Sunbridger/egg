2bd03445346cd9ebbcf108f71ff0aa8a112651d9
Sunbridger <739272884@qq.com>
2021-01-07 21:18:54 +0800

    feat: add 12306

--- app/schedule/checkCarTicket.js
+++ app/schedule/checkCarTicket.js
@@ -34,7 +34,7 @@ const checkCarTicket = async (ctx) => {
             'Cookie': '_uab_collina=160488793265541491548447; JSESSIONID=D1534A49A86DCAD1739B4A82D5CE0571; _jc_save_wfdc_flag=dc; BIGipServerpool_passport=98828810.50215.0000; route=6f50b51faa11b987e576cdb301e545c4; _jc_save_fromStation=%u676D%u5DDE%u4E1C%2CHGH; _jc_save_toStation=%u9E70%u6F6D%2CYTG; _jc_save_fromDate=2021-02-05; _jc_save_toDate=2021-01-07; RAIL_EXPIRATION=1610279023669; RAIL_DEVICEID=q_50Q5qf-rp6oQmhlx9Rjtc4ozMqdgsN_WLSPuZBwmx4YEGF0Idq6LEpOzgtkWGzpJkHt_rdR8raVT1zgGYugDoNtctyLIsJO26acvsZuXwZ0SQZCSLeM0W8yRWR7mXkxOgopwbJh420AlYgZrgWS6cqjGT4ltpn; BIGipServerotn=837812746.50210.0000; BIGipServerportal=3067347210.16671.0000'
         }
     });
-    if (result?.data?.data?.result?.length) {
+    if (result.data.data && result.data.data.result.length) {
         sendMail({
             from: '"乔乔乔小助手" <shuhaozhushou@163.com>',
             to: 'sunbridger@sina.com',
