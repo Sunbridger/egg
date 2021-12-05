570e06fc90e522d7742d112ea11e0ad971ad4a8a
七彩 <wangriqiao@cai-inc.com>
2021-12-05 18:40:28 +0800

    feat: add pushLove

--- app/schedule/pushLove.js
+++ app/schedule/pushLove.js
@@ -1,5 +1,7 @@
 const lovePicsIcon = 'https://img.souche.com/f2e/0b0f755e8e57683c3281102c99c7013f.jpg';
 
+const qiaoBarkURL = 'BkXhvz8ctcMK3nRxsWKXdG';
+
 // 获取情话文案
 const getWhispers = async () => {
 
@@ -15,7 +17,7 @@ module.exports = app => {
         async task(ctx) {
           const textWhispers= await ctx.curl('https://api.vvhan.com/api/love?type=json');
           console.log(textWhispers, '-textWhispers-')
-          await ctx.curl(`https://api.day.app/BkXhvz8ctcMK3nRxsWKXdG/亲爱的文文宝贝：${textWhispers}?icon=${lovePicsIcon}`);
+          await ctx.curl(`https://api.day.app/${qiaoBarkURL}/亲爱的文文宝贝：${textWhispers}?icon=${lovePicsIcon}`);
         }
     }
 };
